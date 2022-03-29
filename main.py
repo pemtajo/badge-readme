3113d3e4782b68970bbd506ca5c982525e454ba0
Alexandre Teyar <11601622+aress31@users.noreply.github.com>
2022-03-29 13:23:45 +0200

    Update main.py
    - Fix extra line break

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def generate_new_readme(md_badges, readme):
     """Generate a new Readme.md"""
     if not md_badges:
         return readme
-    badges_in_readme = f"{START_COMMENT}\n\n{md_badges}\n{END_COMMENT}"
+    badges_in_readme = f"{START_COMMENT}\n{md_badges}\n{END_COMMENT}"
 
     return re.sub(LIST_REGEX, badges_in_readme, readme)
 
